8672ae803a9ca8d6896e94a752c931e7005b0e33
Collins0399 <kiprotichcollince34@gmail.com>
2026-05-28 09:46:00 +0300

    Initial commit

---
+++ src/main/java/com/kilimotech/kilimo_chain_backend/KilimoChainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kilimotech.kilimo_chain_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilimoChainBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KilimoChainBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kilimotech/kilimo_chain_backend/KilimoChainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kilimotech.kilimo_chain_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KilimoChainBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
